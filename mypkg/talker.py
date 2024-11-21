999bc49550849f110a0ee1a501467eafdac47bb7
sakaitaisei <justdoit0430@i.softbank.jp>
2024-11-21 16:01:03 +0900

    9回途中

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,23 +1,19 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Person
+from person_msgs.srv import Query
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Person, "person", 10)
-n = 0
 
 
-def cb():
-    global n
-    msg = Person()
-    msg.name = "酒井大成"
-    msg.age = n
-    pub.publish(msg)
-    n += 1
-
+def cb(request,response):
+    if request.name == "酒井大成":
+        response.age = 46
+    else:
+        response.age = 255
+    
+    return response
 
 def main():
-    node.create_timer(0.5, cb)
+    srv = node.create_service(Query, "query", cb)
     rclpy.spin(node)
-            
