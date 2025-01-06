91181202f693ca82c95d634f1c3ce1a3cb3d506f
sakaitaisei <justdoit0430@i.softbank.jp>
2025-01-06 11:47:52 +0900

    add

--- test/test.bash
+++ test/test.bash
@@ -1,12 +1,19 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2024 TAISEI SAKAI
+# SPDX-License-Identifier: BSD-3-Clause
 
 dir=~
 [ "$1" != "" ] && dir="$1"
 
 cd $dir/ros2_ws
 colcon build
-source $dir/.bashrc
-timeout 10 ros2 launch mypkg talk_listen.launch.py | tee - /tmp/mypkg.log
+source $dir/ros2_ws/install/setup.bash  # ROS 2ワークスペースの環境設定
 
-cat /tmp/mypkg.log |
-grep 'Listen: 10'
+# タイムアウト10秒でtalkerノードを実行し、ログを/tmp/mypkg.logに保存
+timeout 10 ros2 run mypkg talker > /tmp/mypkg.log
+
+# ログファイルから特定の経過時間メッセージが含まれているかをgrepでチェック
+grep '時間' /tmp/mypkg.log
+grep '分' /tmp/mypkg.log
+grep '秒' /tmp/mypkg.log
+
